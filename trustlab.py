566a8c1b8ea04800489997f4096966c58176b0ed
skeeperloyaltie <gugod254@gmail.com>
2023-03-29 10:30:02 +0300

    Bug

--- trustlab.py
+++ trustlab.py
@@ -1,5 +1,5 @@
 def fizzBuzz(n):
-    for i in range(1,n):
+    for i in range(1,n + 1):
         if i % 3 == 0 and i % 5  == 0:
             print('FizzBuzz')
         elif i % 3 == 0 and i % 5 != 0:
