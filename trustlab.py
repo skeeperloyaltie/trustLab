93e93dd7e08d9724031d0383bec4bb35971a648b
skeeperloyaltie <gugod254@gmail.com>
2023-03-29 10:17:31 +0300

    first commit

---
+++ trustlab.py
@@ -0,0 +1,17 @@
+def fizzBuzz(n):
+    for i in range(1,n):
+        if i % 3 == 0 and i % 5  == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0 and i % 5 != 0:
+            print('Fizz')
+        elif i % 5 == 0 and  i % 3 != 0:
+            print('Buzz')
+        elif i % 3 != 0 and i % 5 != 0:
+            print(i)
+        else:
+            raise ValueError
+        
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    fizzBuzz(n)
